798379343feaad5b2eec339dd727d5b1e82d2ae4
zhanzhan <2837420590@qq.com>
2026-05-17 19:48:29 +0800

    功能：重构 Task 类，添加字段和方法以支持任务管理

--- MTB-backend/src/main/java/com/example/mtbbackend/entity/Task.java
+++ MTB-backend/src/main/java/com/example/mtbbackend/entity/Task.java
@@ -1,4 +1,75 @@
 package com.example.mtbbackend.entity;
 
-public class Task {
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.time.LocalDateTime;
+
+@TableName("task")
+public class Task {//数据库表task以及相应字段
+
+    @TableId(type = IdType.INPUT)
+    private String id;//uuid
+    private String title;//任务标题
+    private String description;//任务介绍
+    private Boolean completed;//完成情况
+    private LocalDateTime createTime;//创建时间
+    private LocalDateTime finishTime;//完成时间
+
+    public Task(){
+    }
+    public Task(String title, String description, Boolean completed){
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDateTime getFinishTime() {
+        return finishTime;
+    }
+
+    public void setFinishTime(LocalDateTime finishTime) {
+        this.finishTime = finishTime;
+    }
 }
